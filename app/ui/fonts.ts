d1059226198248bf1e737ea6c887ebcd26b86fbc
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-11-29 15:54:49 +0500

    client side navigation

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,7 +1,22 @@
 import {Inter , Lusitana} from 'next/font/google'
-
+// import localFont from "next/font/local";
 export const inter = Inter ({subsets : ['latin']})
 export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
+
+// export const Inter = localFont({
+//     src: "../../public/fonts/Inter_28pt-Regular.ttf",
+//     variable: "--font-Inter",
+//     weight: "100 900",
+//   });
+// export const Lusitana = localFont({
+//     src: "../../public/fonts/Lusitana-Regular.ttf",
+//     variable: "--font-Lusitana",
+//     weight: "100 900",
+//   });
+
+
+  // import '../../public/fonts/Inter_28pt-Regular.ttf'
+// import '../../public/fonts/Lusitana-Regular.ttf'
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 
+
 export default function Search({ placeholder }: { placeholder: string }) {
   return (
     <div className="relative flex flex-1 flex-shrink-0">
