49ac15cc7f3d7adadfa495edc256f7ed1ebef736
sharjeel-ahmed17 <sharjeelahmedofficial2@gmail.com>
2024-11-29 15:26:58 +0500

    fonts is used

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
-
+// import styles from '@/app/ui/home.module.css'
+import {lusitana} from '@/app/ui/fonts'
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      {/* <h1 className='text-xl bg-red-600'>sharjeel ali bari</h1> */}
+      {/* <p className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"></p> */}
+      {/* <div className={styles.shape}> </div> */}
+      <p
+      className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >sharjeel ali bari</p>
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
